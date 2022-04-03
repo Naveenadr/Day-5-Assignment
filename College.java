54203ae0968b89186c47b5c32479e67779b8387d
Naveena <101961808+Naveenadr@users.noreply.github.com>
2022-04-03 11:35:58 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,23 @@
+package org.college;
+
+public class College {
+	public void collegeName () {
+		System.out.println ("Velammal Engineering college");
+		}
+	public void collegeCode () {
+		System.out.println ("College code is 1132");
+	}
+	
+	public void collegeRank () {
+		System.out.println ("Comes in tier2 ranking");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		//System.out.println ("main");
+	}
+}
+
+		
+
